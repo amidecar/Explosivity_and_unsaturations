0bcc24cb0cca1c1b3c6fd5a8fce98630c6379213
amidecar <ami.decarli@epfl.ch>
2024-05-16 23:15:18 +0200

    added test

--- tests/test_example_module.py
+++ tests/test_example_module.py
@@ -1,6 +1,9 @@
-from explosivity_and_unsaturations.example_module import balox
+from explosivity_and_unsaturations.example_module import balox, insat
 
 
 # Test the function
 def test_hello_smiles():
     assert balox("O=C=O") == 0, "Test failed: SMILES input"
+
+def test_jaaj():
+    assert insat("O=C=O") == 2, "Test failed: wrong value"
